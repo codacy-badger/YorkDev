90f20df3849603205a1c6dca28083cfd62bb8a34
York <aarghgames@gmail.com>
2017-07-31 10:26:54 +0100

    MAOR DEBUG!

--- commands/update.js
+++ commands/update.js
@@ -7,14 +7,11 @@ const { run: reboot } = require('./reboot');
 exports.run = async (client, message, args, level) => {
   const { stdout, stderr, err } = await exec('git pull https://github.com/YorkAARGH/York-Dev.git', { cwd: path.join(__dirname, '../') }).catch(err => ({ err }));
   if (err) return console.error(err);
-  console.log(`stdout:: ${stdout}\n\n`);
-  console.log(`stderr:: ${stderr}\n\n`);
-
   const out = [];
   if (stdout) out.push(stdout);
   if (stderr) out.push(stderr);
-  await message.channel.send(out.join('---\n---'), { code: true });
-  if (stdout.toString() === 'Already up-to-date.') {
+  await message.channel.send(out.join('---\n'), { code: true });
+  if (stdout.toString().includes('Already up-to-date.')) {
     return console.log(true);
   } else {
     return console.log(false);
