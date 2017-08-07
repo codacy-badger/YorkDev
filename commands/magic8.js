8913514a2badefbb128167c21c74b7cfd0ceebdf
York <aarghgames@gmail.com>
2017-08-07 23:24:09 +0100

    Added Command.
    Got bored, added a Magic 8 Ball command.

---
+++ commands/magic8.js
@@ -0,0 +1,24 @@
+const answers = [ 'Maybe.', 'Certainly not.', 'I hope so.', 'Not in your wildest dreams.', 'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.', 'I hope so.', 'Never!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.', 'Sorry, bucko.', 'Hell, yes.', 'Hell to the no.', 'The future is bleak.', 'The future is uncertain.', 'I would rather not say.', 'Who cares?', 'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!' ];
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  if (!message.content.endsWith('?')) return message.reply('That does not look like a question, (hint, end your question with a `?`.)');
+  const msg = await message.channel.send('`Thinking...`');
+  setTimeout( async () => {
+    await msg.edit(`${answers[Math.floor(Math.random() * answers.length)]}`);
+  }, Math.random() * (1 - 5) + 1 * 2000);
+};
+
+exports.conf = {
+  hidden: false,
+  guildOnly: true,
+  aliases: ['8', '8ball'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'magic8',
+  description: 'Answers a question, magic 8 ball style.',
+  usage: 'magic8 <question>',
+  category: 'Fun',
+  extended: 'This command will answer any question given to it in the style of a magic 8 ball.'
+};
