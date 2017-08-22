9647a9a4c29a12fb7e58f5ae4ee4fda44635e82a
York <aarghgames@gmail.com>
2017-08-22 14:43:56 +0100

    Updated Command.
    Updated FML, code supplied by Stitch from the Idiot's Guide Community.

--- commands/fml.js
+++ commands/fml.js
@@ -1,26 +1,45 @@
+/*
+
+  This is a modified FML command thanks to Stitch <@257847417183928320>
+  for supplying the updated code.
+
+*/
 const request = require('snekfetch');
 const HTMLParser = require('fast-html-parser');
 const {RichEmbed} = require('discord.js');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send('`Fetching random story...`');
+  const reply = await message.channel.send('```Searching for a random FML card (this can take a few seconds)```');
   const res = await request.get('http://www.fmylife.com/random');
   const root = HTMLParser.parse(res.text);
   const article = root.querySelector('.block a');
   const downdoot = root.querySelector('.vote-down');
   const updoot = root.querySelector('.vote-up');
+  const href = root.querySelector('.panel-content p.block a');
+  const card = root.querySelector('.panel-content div.votes span.vote div');
+  const signature = root.querySelector('.panel div.text-center');
+  const link = 'http://www.fmylife.com' + href.rawAttrs.replace(/^href=|"/g,'');
+  const cardId = card.rawAttrs.replace(/\D/g,'');
+  let signatureDisplay = 'Author and date of this fml unkown';
+  if (signature.childNodes.length === 1) {
+    signatureDisplay = signature.childNodes[0].text;
+  } else if (signature.childNodes.length === 3) {
+    signatureDisplay = signature.childNodes[0].text.replace('-', '/') + signature.childNodes[2].text.replace('/','');
+  }
+
   const embed = new RichEmbed()
-    .setTitle(`Requested by: ${message.author.username}`)
-    .setAuthor('Fuck My Life, Random Edition!')
-    .setColor(0x32AAE1)
-    .setTimestamp()
+    .setTitle(`FML #${cardId}`)
+    .setURL(link)
+    .setColor(165868)
+    .setThumbnail('http://i.imgur.com/5cMj0fw.png')
+    .setFooter(signatureDisplay)
     .setDescription(`_${article.childNodes[0].text}\n\n_`)
     .addField('I agree, your life sucks', updoot.childNodes[0].text, true)
     .addField('You deserved it:', downdoot.childNodes[0].text, true);
   if (article.childNodes[0].text.length < 5 ) {
-    return msg.edit('Today, something went wrong, so you\'ll have to try again in a few moments. FML');
+    return message.channel.send('Today, something went wrong, so you\'ll have to try again in a few moments. FML');
   }
-  return msg.edit({embed});
+  reply.edit({embed});
 };
 
 exports.conf = {
