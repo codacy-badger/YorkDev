35a32cb3dbb844a539d19e5f3e1258dff1ed5a6b
York <aarghgames@gmail.com>
2017-09-02 22:52:54 +0100

    More Debugging

--- functions/utilities.js
+++ functions/utilities.js
@@ -50,6 +50,7 @@ module.exports = (client) => {
       const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.modRole.toLowerCase());
       if (modRole && message.member.roles.has(modRole.id)) permlvl = 2;
     } catch (e) {
+      console.log(e);
       console.warn(`modRole (${client.settings.get(message.guild.id).modRole}) not present in guild settings for ${message.guild.name} (${message.guild.id}). Skipping Moderator (level 2) check`);
     }
 
@@ -57,6 +58,7 @@ module.exports = (client) => {
       const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.adminRole.toLowerCase());
       if (adminRole && message.member.roles.has(adminRole.id)) permlvl = 3;
     } catch (e) {
+      console.log(e);
       console.warn(`adminRole (${client.settings.get(message.guild.id).adminRole}) not present in guild settings for ${message.guild.name} (${message.guild.id}). Skipping Administrator (level 3) check`);
     }
 
