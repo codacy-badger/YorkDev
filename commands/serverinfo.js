46230dc8df1dc4850f3cb8a7cd6690cdfffe0171
York <aarghgames@gmail.com>
2017-09-07 22:28:02 +0100

    Codacy Fixes.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -15,16 +15,16 @@ class ServerInfo extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const embed = new RichEmbed()
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .setColor(3447003)
-    .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-    .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
-    .addField('Location', message.guild.region, true)
-    .addField('Created', message.guild.createdAt.toLocaleString(), true)
-    .addField('Roles', message.guild.roles.size, true)
-    .addBlankField(true)
-    .setTimestamp()
-    .setFooter(this.client.user.username, this.client.user.avatarURL);
+      .setAuthor(message.guild.name, message.guild.iconURL)
+      .setColor(3447003)
+      .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
+      .addField('Member Count', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`, true)
+      .addField('Location', message.guild.region, true)
+      .addField('Created', message.guild.createdAt.toLocaleString(), true)
+      .addField('Roles', message.guild.roles.size, true)
+      .addBlankField(true)
+      .setTimestamp()
+      .setFooter(this.client.user.username, this.client.user.avatarURL);
     message.channel.send({embed}).catch(e => console.error(e));
   }
 }
