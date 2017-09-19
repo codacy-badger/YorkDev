d90ded81508843f38b33baa0d308ac62f6dc68fd
York <aarghgames@gmail.com>
2017-09-19 07:48:49 +0100

    Regex cleanup

--- monitors/starboard.js
+++ monitors/starboard.js
@@ -42,11 +42,11 @@ class Starboard {
 
     if (starChannel.messages.size < 100) await starChannel.fetchMessages({ limit: 100 });
     if (message.channel.type !== 'text' || reaction.emoji.name !== '⭐') return;
-    if (message.author.id === user.id) {
-      return message.channel.send(`${user}, you cannot star your own messages.`).then(() => {
-        reaction.remove(user.id);
-      });
-    }
+    // if (message.author.id === user.id) {
+    //   return message.channel.send(`${user}, you cannot star your own messages.`).then(() => {
+    //     reaction.remove(user.id);
+    //   });
+    // }
     if (message.author.bot) {
       return message.channel.send(`${user}, you cannot star bot messages.`).then(() => {
         reaction.remove(user.id);
@@ -63,7 +63,7 @@ class Starboard {
     try {
       const stars = starChannel.messages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
       if (stars) {
-        const star = /\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/g.exec(stars.embeds[0].footer.text);
+        const star = /⭐ ([0-9]{1,3}) \| ([0-9]{17,20})/g.exec(stars.embeds[0].footer.text);
         const _star = stars.embeds[0];
         const embed = starEmbed(_star.color, _star.description, _star.author.name, _star.thumbnail.url, _star.createdTimestamp, `⭐ ${parseInt(star[1])+1} | ${message.id}`, _star.image.url);
         const starMsg = await starChannel.fetchMessage(stars.id);
