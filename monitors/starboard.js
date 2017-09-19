a6ad75be606f0c1eec3daeb0781938345897e316
York <aarghgames@gmail.com>
2017-09-19 18:54:44 +0100

    Fixed? Starboard
    Removed the ability to star messages with attachments.

--- monitors/starboard.js
+++ monitors/starboard.js
@@ -1,5 +1,5 @@
 
-function starEmbed(color, description, author, thumbnail, timestamp, footer, image = null) {
+function starEmbed(color, description, author, thumbnail, timestamp, footer) {
   const embed = {
     'color': color,
     'description': description,
@@ -9,22 +9,12 @@ function starEmbed(color, description, author, thumbnail, timestamp, footer, ima
     'thumbnail': {
       'url': thumbnail
     },
-    'image': {
-      'url': image
-    },
     'timestamp': timestamp,
     'footer': {
       'text': footer
     }
   };
   return embed;
-}
-function extention(reaction, attachment) {
-  const imageLink = attachment.split('.');
-  const typeOfImage = imageLink[imageLink.length - 1];
-  const image = /(jpg|jpeg|png|gif)/gi.exec(typeOfImage);
-  if (!image) return '';
-  return attachment;
 }
 
 class Starboard {
@@ -41,21 +31,23 @@ class Starboard {
     const starChannel = message.guild.channels.find('name', starboard);
 
     if (starChannel.messages.size < 100) await starChannel.fetchMessages({ limit: 100 });
+    
     if (message.channel.type !== 'text' || reaction.emoji.name !== '⭐') return;
+
     if (message.author.id === user.id) {
       return message.channel.send(`${user}, you cannot star your own messages.`).then(() => {
         reaction.remove(user.id);
       });
     }
+    
     if (message.author.bot) {
       return message.channel.send(`${user}, you cannot star bot messages.`).then(() => {
         reaction.remove(user.id);
       });
     }
 
-    const image = message.attachments.size > 0 ? await extention(reaction, message.attachments.array()[0].url) : '';
-    if (image === '' && message.cleanContent.length < 1) {
-      return message.channel.send(`${user}, you cannot star an empty message.`).then(() => {
+    if (message.attachments.size > 0) {
+      return message.channel.send(`${user}, you cannot star a message with attachments.`).then(() => {
         reaction.remove(user.id);
       });
     }
@@ -65,13 +57,13 @@ class Starboard {
       if (stars) {
         const star = /⭐ ([0-9]{1,3}) \| ([0-9]{17,20})/g.exec(stars.embeds[0].footer.text);
         const _star = stars.embeds[0];
-        const embed = starEmbed(_star.color, _star.description, _star.author.name, _star.thumbnail.url, _star.createdTimestamp, `⭐ ${parseInt(star[1])+1} | ${message.id}`, _star.image.url);
+        const embed = starEmbed(_star.color, _star.description, _star.author.name, _star.thumbnail.url, _star.createdTimestamp, `⭐ ${parseInt(star[1])+1} | ${message.id}`);
         const starMsg = await starChannel.fetchMessage(stars.id);
         await starMsg.edit({ embed });
       }
       if (!stars) {
         if (!message.guild.channels.exists('name', starboard)) throw `It appears that you do not have a \`${starboard}\` channel.`;
-        const embed = starEmbed(15844367, message.cleanContent, message.author.tag, message.author.displayAvatarURL, new Date(), `⭐ 1 | ${message.id}`, `${image}`);
+        const embed = starEmbed(15844367, message.cleanContent, message.author.tag, message.author.displayAvatarURL, new Date(), `⭐ 1 | ${message.id}`);
         await starChannel.send({ embed });
       }
     } catch (error) {
