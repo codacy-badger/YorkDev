cd0f588bb33773a0bd047ae4584b896ebdefac8a
Amish Shah <amishshah.2k@gmail.com>
2017-10-24 00:19:35 +0100

    Remove unnecessary async/await

--- commands/fun/magic8.js
+++ commands/fun/magic8.js
@@ -23,9 +23,10 @@ class Magic8 extends Social {
       const payMe = await this.cmdPay(message, message.author.id, cost, this.conf.botPerms);
       if (!payMe) return;  
       const msg = await message.channel.send('`Thinking...`');
-      setTimeout(async () => {
-        await msg.edit(`${answers[Math.floor(Math.random() * answers.length)]}`);
-      }, Math.random() * (1 - 5) + 1 * 2000);
+      setTimeout(
+        () => msg.edit(`${answers[Math.floor(Math.random() * answers.length)]}`,
+        Math.random() * (1 - 5) + 1 * 2000)
+      );
     } catch (error) {
       throw error;
     }
