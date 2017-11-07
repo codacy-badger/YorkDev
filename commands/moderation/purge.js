a3f8baefe3fa49292fce102bc4ef0e128ba76e4a
York <aarghgames@gmail.com>
2017-11-07 23:30:22 +0000

    Updated Purge
    Removes messages from cache before manually triggering the bulkDelete event.

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -8,22 +8,29 @@ class Purge extends Moderation {
       usage: 'purge [user] <number>',
       extended: 'This command will either purge a mentioned users messages (between 2 and 99), or the bots own messages.',
       aliases: ['prune'],
-      botPerms: ['SEND_MESSAGES', 'MANAGE_MESSAGES']
+      botPerms: ['MANAGE_MESSAGES']
     });
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
+    
     const user = message.mentions.users.first();
     const amount = parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2]);
+
     if (!amount) return message.reply('Must specify an amount to delete!');
     if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-    message.channel.fetchMessages({ limit: amount }).then((messages) => {
-      if (user) {
-        const filterBy = user ? user.id : this.client.user.id;
-        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-      }
-      message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-    });  
+
+    let messages = await message.channel.fetchMessages({ limit: amount });
+
+    if (user) {
+      const filterBy = user ? user.id : this.client.user.id;
+      messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+    }
+
+    this.client.emit('messageDeleteBulk', messages);
+    for (const msg of messages.values()) msg.channel.messages.delete(msg.id);
+    
+    message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
   }
 }
 
