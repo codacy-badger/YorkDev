2e77f7e8cd654a61125112316ec13611a3365f55
Leon <leonbohm@gmail.com>
2018-01-14 13:47:12 +0000

    stuff, and things

--- monitors/linting.js
+++ monitors/linting.js
@@ -1,9 +1,6 @@
 const lint = require('../functions/linting/lint.js');
-const lang = require('lang-detector');
 exports.run = async (client, message, level) => { // eslint-disable-line no-unused-vars
-  if (message.content.match(/```js\s(.|\s)+```/gi)) {
-    lint(message);
-  } else if (message.content.match(/```(.|\s)+```/gi) && lang(message.content.replace(/```(js)?|```/gi, '')) === 'JavaScript') {
+  if (message.content.match(/```[^`]+```/gi)) {
     lint(message);
   }
 };
